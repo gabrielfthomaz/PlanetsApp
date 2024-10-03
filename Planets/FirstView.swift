a9c53a6dafcf86ce3db8b47218ed2b82ddcaf2ff
Gabriel Thomaz <gabrielfthomaz@hotmail.com>
2024-10-03 10:22:13 -0300

    Ajuste do layout da tela de planetas e inicio da criação da tela principal

---
+++ Planets/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  Planets
+//
+//  Created by Gabriel Fernandes Thomaz on 03/10/24.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstView()
+}
